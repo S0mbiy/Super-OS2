2e07b3a9ee9771ee325f00f12d9878d161f8d552
Sergio Alvarado <alvaradoomar55@hotmail.com>
2019-02-20 03:08:18 +0000

    practica 2

--- sh.c
+++ sh.c
@@ -145,8 +145,12 @@ int
 main(void)
 {
   static char buf[100];
+	static char preBuf[100];
   int fd;
+	int i;
 
+	
+	preBuf[0] = '\0';
   // Ensure that three file descriptors are open.
   while((fd = open("console", O_RDWR)) >= 0){
     if(fd >= 3){
@@ -157,6 +161,14 @@ main(void)
 
   // Read and run input commands.
   while(getcmd(buf, sizeof(buf)) >= 0){
+	  if(buf[0]== 'p' && buf[1]== '\n'){
+	  	if(preBuf[0]=='\0')continue;
+		  if(fork1()==0)runcmd(parsecmd(preBuf));
+		  wait();
+	}
+	else{
+
+	
     if(buf[0] == 'c' && buf[1] == 'd' && buf[2] == ' '){
       // Chdir must be called by the parent, not the child.
       buf[strlen(buf)-1] = 0;  // chop \n
@@ -164,11 +176,16 @@ main(void)
         printf(2, "cannot cd %s\n", buf+3);
       continue;
     }
-    if(fork1() == 0)
-      runcmd(parsecmd(buf));
-    wait();
+    if(fork1() == 0) runcmd(parsecmd(buf));
+      for(i=0;buf[i]!='\0';i++){
+	     preBuf[i]=buf[i];
+	  }
+		preBuf[i]='\0';
+		wait();
+  }
   }
   exit();
+
 }
 
 void
